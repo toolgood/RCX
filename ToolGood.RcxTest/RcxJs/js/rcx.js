878abe7ebdc63ab692bc9ec9706bf075f1805f29
toolgood <toolgood@qq.com>
2019-01-02 22:10:56 +0800

    修改 rcx.js

--- ToolGood.RcxTest/RcxJs/js/rcx.js
+++ ToolGood.RcxTest/RcxJs/js/rcx.js
@@ -1,6 +1,4 @@
-﻿
-
-function rcx(data, pass) {
+﻿function rcx(data, pass) {
     function GetKey(pass, klen) {
         var mBox = [];
         for (var i = 0; i < klen; i++) {
@@ -100,7 +98,7 @@ function byteToString(arr) {
 function arrayToHex(data) {
     var val = "";
     for (var i = 0; i < data.length; i++) {
-        val += data[i].toString(16);
+        val += data[i].toString(16).toUpperCase();
     }
     return val;
 }
@@ -113,69 +111,82 @@ function hexToArray(str) {
     return data;
 }
 
-var b64map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-var b64padchar = "=";
-
-function hex2b64(h) {
-    var i;
-    var c;
-    var ret = "";
-    for (i = 0; i + 3 <= h.length; i += 3) {
-        c = parseInt(h.substring(i, i + 3), 16);
-        ret += b64map.charAt(c >> 6) + b64map.charAt(c & 63);
-    }
-    if (i + 1 == h.length) {
-        c = parseInt(h.substring(i, i + 1), 16);
-        ret += b64map.charAt(c << 2);
-    }
-    else if (i + 2 == h.length) {
-        c = parseInt(h.substring(i, i + 2), 16);
-        ret += b64map.charAt(c >> 2) + b64map.charAt((c & 3) << 4);
-    }
-    while ((ret.length & 3) > 0) ret += b64padchar;
-    return ret;
-}
-function b64tohex(s) {
-    var ret = ""
-    var i;
-    var k = 0; // b64 state, 0-3
-    var slop;
-    for (i = 0; i < s.length; ++i) {
-        if (s.charAt(i) == b64padchar) break;
-        v = b64map.indexOf(s.charAt(i));
-        if (v < 0) continue;
-        if (k == 0) {
-            ret += int2char(v >> 2);
-            slop = v & 3;
-            k = 1;
-        }
-        else if (k == 1) {
-            ret += int2char((slop << 2) | (v >> 4));
-            slop = v & 0xf;
-            k = 2;
-        }
-        else if (k == 2) {
-            ret += int2char(slop);
-            ret += int2char(v >> 2);
-            slop = v & 3;
-            k = 3;
-        }
-        else {
-            ret += int2char((slop << 2) | (v >> 4));
-            ret += int2char(v & 0xf);
-            k = 0;
-        }
-    }
-    if (k == 1)
-        ret += int2char(slop << 2);
-    return ret;
-}
-function int2char(num) {
-  return  String.fromCharCode(num);
-}
 function arrayToBase64(data) {
-    return hex2b64(arrayToHex(data))
+    var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var out, i, len;
+    var c1, c2, c3;
+    len = data.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        c1 = data[i++] & 0xff;
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = data[i++];
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = data[i++];
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
 }
 function base64ToArray(str) {
-    return hexToArray(b64tohex(str))
-}
+    var base64DecodeChars = new Array(
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+        -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+
+    var c1, c2, c3, c4;
+    var len = str.length;
+    var i = 0;
+    var out = [];
+    while (i < len) {
+        do {
+            c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        } while (i < len && c1 == -1);
+        if (c1 == -1)
+            break;
+        do {
+            c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        } while (i < len && c2 == -1);
+        if (c2 == -1)
+            break;
+        out.push((c1 << 2) | ((c2 & 0x30) >> 4));
+        do {
+            c3 = str.charCodeAt(i++) & 0xff;
+            if (c3 == 61)
+                return out;
+            c3 = base64DecodeChars[c3];
+        } while (i < len && c3 == -1);
+        if (c3 == -1)
+            break;
+        out.push(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+        do {
+            c4 = str.charCodeAt(i++) & 0xff;
+            if (c4 == 61)
+                return out;
+            c4 = base64DecodeChars[c4];
+        } while (i < len && c4 == -1);
+        if (c4 == -1)
+            break;
+        out.push(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+};
